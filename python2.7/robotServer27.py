e3367a188b22f932810333195781db3d3da4012c
Antonello Meloni <infovillasimius@gmail.com>
2020-12-25 12:28:12 +0100

    Alexa skill
    Added initial code of  "Zora Robot" Alexa skill

---
+++ python2.7/robotServer27.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import qi
+import argparse
+import socket
+import sys
+from balla import balla
+from saluta import saluta
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--ip", type=str, default="127.0.0.1",
+                    help="Robot IP address. On robot or Local Naoqi: use '127.0.0.1'.")
+parser.add_argument("--port", type=int, default=9559,
+                    help="Naoqi port number")
+
+args = parser.parse_args()
+session = qi.Session()
+try:
+    session.connect("tcp://" + args.ip + ":" + str(args.port))
+except RuntimeError:
+    print ("Can't connect to Naoqi at ip \"" + args.ip + "\" on port " + str(args.port) +".\n"
+           "Please check your script arguments. Run with -h option for help.")
+    sys.exit(1)
+
+posture_service = session.service("ALRobotPosture")
+posture_service.goToPosture("StandInit", 1.0)
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Bind the socket to the port
+server_address = ('192.168.1.125', 10000)
+print >>sys.stderr, 'starting up on %s port %s' % server_address
+sock.bind(server_address)
+sock.listen(1)
+
+while True:
+    # Wait for a connection
+    print >>sys.stderr, 'waiting for a connection'
+    connection, client_address = sock.accept()
+    try:
+        print >>sys.stderr, 'connection from', client_address
+
+        # Receive the data   
+        cmd = connection.recv(3)
+        print >>sys.stderr, 'received "%s"' % cmd
+        
+            
+    finally:
+        # Clean up the connection
+        connection.close()
+        
+
+    if cmd =='sit':
+        posture_service.goToPosture("Sit", 1.0)
+    elif cmd =='sta':
+        posture_service.goToPosture("Stand", 1.0)
+    elif cmd =='cro':
+        posture_service.goToPosture("Crouch", 1.0)
+    elif cmd =='bal':
+        balla()
+    elif cmd =='sal':
+        posture_service.goToPosture("Stand", 1.0)
+        saluta()
+        
+
+    print (posture_service.getPostureFamily())
+
+    
+
+
+
+
---
+++ python3.9/httpServer.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+import socket
+import sys
+
+
+
+class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        query_components = parse_qs(urlparse(self.path).query)
+        path = urlparse(self.path).path
+        print (query_components)
+        print (path)
+        password = query_components.get('pass')
+        cmd = query_components.get('cmd')
+        print (password)
+        print (cmd)
+        
+        if cmd is not None:
+            cmd = cmd[0]
+            
+        print (cmd)
+        
+        if path != '/' or password is None or '123abc' not in password:
+            self.send_response(401)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            message = "Unauthorized!"
+            self.wfile.write(bytes(message, "utf8"))
+            return
+
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+
+        if cmd=='sal':
+            message='Zora Robot ti saluta'
+        else:
+            message = "Ok!"
+
+        self.wfile.write(bytes(message, "utf8"))
+
+        # Create a TCP/IP socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_address = ('192.168.1.125', 10000)
+        sock.settimeout(1)
+        sock.connect(server_address)
+
+        try:
+            message = (cmd[:3]).encode('utf-8')
+            sock.sendall(message)
+
+        except:
+            print("Socket error")
+            sock.close()
+            return
+
+        finally:
+            print ('closing socket')
+            sock.close()
+
+        return
+
+
+def run():
+    print('Avvio del server...')
+    # hostname = socket.gethostname()
+    # local_ip = socket.gethostbyname(hostname)
+    local_ip = '192.168.1.125'
+    server_address = (local_ip, 8008)
+    httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
+    print('Server in esecuzione...')
+    print('Ip = ',local_ip)
+    httpd.serve_forever()
+
+
+run()
